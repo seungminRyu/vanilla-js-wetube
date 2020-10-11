dbcb9fc5b25aa918f7966caa74ae9bb89a50e7cd
smryu <fbtmdals011@gmail.com>
2020-10-12 00:51:16 +0900

    1012 app.listen

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hi!');
+const express = require("express");
+const app = express();
+
+function linstenHandler() {
+    console.log(`http://localhost:4000`);
+}
+
+app.listen(4000, linstenHandler);
