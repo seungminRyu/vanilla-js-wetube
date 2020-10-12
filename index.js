dbf6c75e4f088ae5c1bba1ef01a63862251f5450
smryu <fbtmdals011@gmail.com>
2020-10-12 15:26:14 +0900

    1012 바벨 적용 완료

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 
 const handleHome = (req, res) => res.send("Hi, Welcome to my home");
 
+const handleProfile = (req, res) => res.send("Here is profile~!");
+
 const linstenHandler = () =>
     console.log(`http://localhost:4000`);
 
 app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(4000, linstenHandler);
