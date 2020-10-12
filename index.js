3e070dd68797fa70bec84e5bc65213108791a400
smryu <fbtmdals011@gmail.com>
2020-10-12 16:10:39 +0900

    1012 미들웨어 적용

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const handleHome = (req, res) => res.send("Hi, Welcome to my home");
-
 const handleProfile = (req, res) => res.send("Here is profile~!");
 
 const linstenHandler = () =>
     console.log(`http://localhost:4000`);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
