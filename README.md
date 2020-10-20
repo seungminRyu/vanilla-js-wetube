62d2b3c0935a3996d3a8014fa9ca7b32090e6430
smryu <fbtmdals011@gmail.com>
2020-10-20 11:52:37 +0900

    1020 로그인/회원가입 구현

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # wetube_2020
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const logIn = (req, res) => res.send("Log In");
-export const logOut = (req, res) => res.send("Log Out");
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User Detail");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const logIn = (req, res) => res.render("logIn", { pageTitle: "Log In" });
+export const logOut = (req, res) => res.render("logOut", { pageTitle: "Log Out" });
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
