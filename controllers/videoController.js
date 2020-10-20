9db7a8b66cefbebc3a5a795aa91a6aa283ca18bd
smryu <fbtmdals011@gmail.com>
2020-10-20 10:20:58 +0900

    1020 검색 로직 적용

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,12 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home"});
-export const search = (req, res) => res.send("search", { pageTitle: "Search"});
-export const videos = (req, res) => res.send("Videos", { pageTitle: "Videos"});
-export const upload =  (req, res) => res.send("Upload", { pageTitle: "Upload"});
-export const videoDetail = (req, res) => res.send("Video Detail", { pageTitle: "Video Detail"});
-export const editVideo = (req, res) => res.send("Edit Video", { pageTitle: "Edit Video"});
-export const deleteVideo = (req, res) => res.send("Delete Video", { pageTitle: "Delete Video"});
+
+export const search = (req, res) => {
+    const { query: { term: searchingBy } } = req;
+    res.render("search", { pageTitle: "Search", searchingBy: searchingBy})
+};
+
+export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos"});
+export const upload =  (req, res) => res.render("Upload", { pageTitle: "Upload"});
+export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail"});
+export const editVideo = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video"});
+export const deleteVideo = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video"});
