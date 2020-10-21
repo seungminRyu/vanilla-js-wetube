068ef5d3969e2ffad3aa56b55383c3c6726c8ae1
smryu <fbtmdals011@gmail.com>
2020-10-21 11:58:24 +0900

    1021 getjoin, postjoin, search 적용

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,7 +4,11 @@ export const home = (req, res) => res.render("home", { pageTitle: "Home", videos
 
 export const search = (req, res) => {
     const { query: { term: searchingBy } } = req;
-    res.render("search", { pageTitle: "Search", searchingBy: searchingBy})
+    res.render("search", { 
+        pageTitle: "Search", 
+        searchingBy: searchingBy, 
+        videos
+    })
 };
 
 export const upload =  (req, res) => res.render("upload", { pageTitle: "Upload"});
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { join, logIn, logOut } from "../controllers/userController";
+import { getJoin, join, logIn, logOut, postJoin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
 globalRouter.get(routes.login, logIn);
 globalRouter.get(routes.logout, logOut);
 globalRouter.get(routes.search, search);
