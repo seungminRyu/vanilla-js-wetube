336472c3fb2dbb05eb5310d53bd1f46b5ad185ff
smryu <fbtmdals011@gmail.com>
2020-10-22 10:19:10 +0900

    1022 로그아웃 적용

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,7 +17,9 @@ export const getLogIn = (req, res) =>
 export const postLogIn = (req, res) => 
     res.redirect(routes.home);
 
-export const logOut = (req, res) => res.render("logout", { pageTitle: "Log Out" });
+export const logOut = (req, res) => 
+    res.redirect(routes.home);
+
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
