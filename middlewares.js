94a060a76976a91759988e2c475838814917e2b1
smryu <fbtmdals011@gmail.com>
2020-10-26 14:10:37 +0900

    1026 multer 적용, video upload

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,8 @@
 import routes from "./routes";
+import multer from "multer";
+
+const multerVideo = multer({ dest: "videos/" });
+export const uploadVideo = multerVideo.single("videoFile");
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "RyuTube";
